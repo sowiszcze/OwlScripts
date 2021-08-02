f2d88b32e18469a477466dbfdd25f3aef8f40726
Piotr Sowa (PIOA) <pioa@demant.com>
2021-08-02 12:53:09 +0200

    Created file for LinkedIn My Learning userscript

---
+++ linkedin.learning.me.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         LinkedIn's My Learning list organizer
+// @version      0.1
+// @description  Userscript allowing to sort and filter in-progress, saved and completed courses.
+// @author       @Sowiszcze
+// @match        http*://linkedin.com/learning/me/in-progress
+// @match        http*://linkedin.com/learning/me/saved
+// @match        http*://linkedin.com/learning/me/completed
+// @match        http*://*.linkedin.com/learning/me/in-progress
+// @match        http*://*.linkedin.com/learning/me/saved
+// @match        http*://*.linkedin.com/learning/me/completed
+// @require      https://code.jquery.com/jquery-3.6.0.slim.min.js
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.log
+// @namespace    https://sowiszcze.it
+// @downloadURL  https://github.com/sowiszcze/owlscripts/linkedin.learning.me.user.js
+// @homepageURL  https://github.com/sowiszcze/owlscripts
+// @icon         https://www.google.com/s2/favicons?domain=learning.linkedin.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM.log('Here I am!');
+})();
